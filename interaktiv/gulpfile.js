c439f61a46a3e7a053f04749d115458d8a69c68f
rasif <19rustamov1996@gmail.com>
2021-03-14 22:11:21 +0400

    gulp

--- interaktiv/gulpfile.js
+++ interaktiv/gulpfile.js
@@ -1,14 +1,43 @@
-const {src, dest} = require('gulp');
+const gulp = require('gulp');
+const concat = require('gulp-concat');
 const pug = require('gulp-pug');
 
-function usePug() {
-	return src('./pug/*.pug')
+var paths = {
+	styles: {
+		src: 'css/*.css',
+		dest: 'css/dest'
+	},
+	pugs: {
+		src: './pug/*.pug',
+		dest: './'
+	}
+};
+
+function styles() {
+	return gulp.src(paths.styles.src).pipe(concat('style.css')).pipe(gulp.dest(paths.styles.dest));
+}
+
+function pugs() {
+	return gulp
+		.src(paths.pugs.src)
 		.pipe(
 			pug({
 				pretty: true
 			})
 		)
-		.pipe(dest('./'));
+		.pipe(gulp.dest(paths.pugs.dest));
 }
 
-exports.usePug = usePug;
+function watch() {
+	gulp.watch(paths.styles.src, styles);
+	gulp.watch(paths.pugs.src, pugs);
+}
+
+const build = gulp.series(gulp.parallel(pugs, styles));
+
+exports.styles = styles;
+exports.pugs = pugs;
+exports.watch = watch;
+exports.build = build;
+
+exports.default = build;
